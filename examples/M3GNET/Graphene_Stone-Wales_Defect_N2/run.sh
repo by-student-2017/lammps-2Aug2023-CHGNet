129a2872887ed4b8e069a89c7df636a938892ca2
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-06-25 16:48:19 +0900

    Add files via upload

---
+++ examples/M3GNET/Graphene_Stone-Wales_Defect_N2/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NCPU=8
+lammps_adress=/mnt/d/lammps
+
+mkdir cfg
+
+export OMP_NUM_THREADS=${NCPU}
+
+# non-OPENMP package installation case
+#${lammps_adress}/src/lmp_serial -in in.lmp
+
+# OPENMP package installation case
+${lammps_adress}/src/lmp_serial -sf omp -pk omp ${NCPU} -in in.lmp
+
